4c0b8f00a8a7caff385c1e2eb6225fec45a75154
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-03-25 21:49:09 -0400

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,19 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Madimi+One&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Madimi+One&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍜</text></svg>"
+        ></link>
       </Head>
       <body>
         <Main />
